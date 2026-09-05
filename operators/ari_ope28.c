35fd290ec5e4296676018412d010f5f2856c99f7
Korukonda Girishkumar <korukondagirishkumar04@gmail.com>
2026-09-05 16:57:50 +0530

    Delete operators directory

--- operators/ari_ope28.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	int num;
-	printf("enter the value");
-	scanf("%d",&num);
-	printf("remainder:%d\n",num%5);
-	return 0;
-}
--- operators/spec_ope63.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	int num=3;
-	printf("preincrement:%d\n",++num);
-	printf("postincrement:%d\n",num++);
-	return 0;
-}
