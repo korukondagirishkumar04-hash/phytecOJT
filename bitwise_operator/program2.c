7d50aca035e4579910f2e946957b736ebaf5718d
Korukonda Girishkumar <korukondagirishkumar04@gmail.com>
2026-09-05 16:58:08 +0530

    Delete bitwise_operator directory

--- bitwise_operator/program2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-	unsigned char dip_switch=0XA7;
-	unsigned char lower_nibble=dip_switch & 0X0F;
-		printf("lower 4 bit : %X(decimal %d)\n",lower_nibble,lower_nibble);
-	return 0;
-	}
