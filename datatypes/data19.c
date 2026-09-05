ebd853e51e4c991ba39986a3c4c62c903b4b72ff
Korukonda Girishkumar <korukondagirishkumar04@gmail.com>
2026-09-05 16:58:36 +0530

    Delete datatypes directory

--- datatypes/data19.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	int a=299;
-	char ch=(char)a;
-	printf("integer:%d\n",a);
-	printf("after casting:%c\n",ch);
-	return 0;
-}
--- datatypes/data24.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	int a=89;
-	int b=87;
-	printf("integerdivision:%d,%d",a,b);
-	printf("after casting:%f",(float)a/b);
-	return 0;
-}
